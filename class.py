6d8905327b18918f092077b0351d205975a60565
valbrecht <vincentalbrecht@outlook.com>
2020-08-17 09:25:37 +0200

    - SILAC filtering - plotting present - LFQ processing - backup before documentation

--- class.py
+++ class.py
@@ -3,18 +3,27 @@ class SpatialDataSet:
 
     def __init__(self, filename):
         """reading the data considering the seperation-mode and comments"""
+        self.filename = filename
+        self.df_long = pd.read_csv(filename, sep="\t", comment="#")
         self.df = pd.read_csv(filename, sep="\t", comment="#")
         self.regex = {
-            "index_col": "[Pp]rotein.[Ii][Dd]s|[Mm]ajority.[Pp]rotein.[Ii][Dd]s|[Pp]rotein.[Nn]ames|[Gg]ene.[Nn]ames|[Ii][Dd]",
-            "Map": ".*([Mm][Aa][pP]\d+).*",
-            "Fraction": ".*_(\d+[Kk])",
-            "Type": "(.*).[Mm][Aa][pP].*",
+            "index_col_silac": "[Pp]rotein.[Ii][Dd]s|[Mm]ajority.[Pp]rotein.[Ii][Dd]s|[Pp]rotein.[Nn]ames|[Gg]ene.[Nn]ames|[Ii][Dd]",
+            "Map_silac": ".*([Mm][Aa][pP]\d+).*",
+            "Fraction_silac": ".*_(\d+[Kk])",
+            "Type_silac": "(.*).[Mm][Aa][pP].*",
             "filter1": "[Pp]otential.[cC]ontaminant",
             "filter2": "[Oo]nly.[iI]dentified.[bB]y.[sS]ite",
-            "filter3": "[Rr]everse"}
+            "filter3": "[Rr]everse",
+
+            "col_shortened" : "id$|[Mm][Ss].*[cC]ount.+$|[Ll][Ff][Qq].*|.*[nN]ames.*|.*[Pp][rR].*[Ii][Dd]s.*|[Pp]otential.[cC]ontaminant|[Oo]nly.[iI]dentified.[bB]y.[sS]ite|[Rr]everse|[Ss]core|[Qq]-[Vv]alue",
+            "index_col_lfq" : ".*[Pp][rR].*[Ii][Dd]s.*|.*[nN]ames.*|[Ii][Dd]|[Ss]core|[Qq]-[Vv]alue|MS/MS.count$",
+            "LFQ_nan": "[Ll][Ff][Qq].*",
+            "Map_lfq": ".*[nNTt]{1}[yYtT]{1}[ .](.*)_\d+[Kk]$",
+            "Fraction_lfq": ".*_(\d+[Kk])",
+            "Type_lfq": "(.*[nNTt]{1}[yYtT]{1})[ .].*_\d+[Kk]$"        }
         self.markerproteins = {"Proteasome": ["PSMA1", "PSMA2", "PSMA3", "PSMA4", "PSMA5", "PSMA6", "PSMA7", "PSMB1",
                                               "PSMB2", "PSMB3", "PSMB4", "PSMB5", "PSMB6", "PSMB7"]}
-
+        self.lfq_filter_param={"summed MS/MS counts" : 2, "consecutive LFQ_I" : 4}
 
     def analyze(self, acquisition):
         """
@@ -54,16 +63,16 @@ class SpatialDataSet:
 
             df_index = df_filt.copy()
             # es wird eine Kopie des Dataframes angelegt
-            col_to_index = [col for col in df_filt.columns if re.match(regex["index_col"], col)]
+            col_to_index = [col for col in df_filt.columns if re.match(regex["index_col_silac"], col)]
             # pro column, fuer jede re: schaut ob spaltenname zu re passt: list out of 1*true and x*false,
             df_index = df_index.set_index(col_to_index)
             # es werden die colum names als index festgelegt
             df_index = df_index.drop([column for column in df_index.columns if not column.startswith("Ratio")], axis=1)
 
             columns = pd.MultiIndex.from_arrays([
-                [re.findall(regex["Map"], colum)[0] for colum in df_index.columns],
-                [re.findall(regex["Fraction"], colum)[0] for colum in df_index.columns],
-                [re.findall(regex["Type"], colum)[0] for colum in df_index.columns]],
+                [re.findall(regex["Map_silac"], colum)[0] for colum in df_index.columns],
+                [re.findall(regex["Fraction_silac"], colum)[0] for colum in df_index.columns],
+                [re.findall(regex["Type_silac"], colum)[0] for colum in df_index.columns]],
                 names=["Map", "Fraction", "Type"])
             df_index.columns = columns
 
@@ -84,21 +93,124 @@ class SpatialDataSet:
             df_final = df_filt3[["Ratio H/L count", "Ratio H/L variability [%]"]].join(pd.DataFrame
                                                                                        (df_norm2.stack("Fraction"),
                                                                                         columns=["Ratio H/L"]))
+            df_final.columns = [col if col != "Ratio H/L" else "normalized profile" for col in df_final.columns]
             return df_final
 
+
+        def filterdf_lfq(df, regex):
+            """"filtering the data in terms of removing entries caracterized by potential contaminants, etc.
+
+            Args:
+                df: dataframe
+                regex: dictionary, in which the values represent the regular expressions
+            """
+            df_filt = df.loc[df[[col for col in df.columns if bool(re.match(regex["filter1"], col))][0]] != "+"]
+            df_filt = df_filt.loc[df_filt[[col for col in df_filt.columns if
+                                           bool(re.match(regex["filter2"], col))][0]] != "+"]
+            df_filt = df_filt.loc[df_filt[[col for col in df_filt.columns if
+                                           bool(re.match(regex["filter3"], col))][0]] != "+"]
+
+            return df_filt
+
+        def readingdf_lfq(filename, df_long, regex):
+            """Creation of a data frame, which contains only desired information
+
+            Args:
+                filename: Data, which is supposed to be analysed
+                df_long: original data frame, containing all information ("Protein groups")
+                regex: regular expression as dictionary: Key (col_shortened) refers to desired column names
+
+            Returns:
+                df_shortened: shortened data frame, contains only the information, which was determined via the Values of the Key col_shortened)
+                """
+
+            col_list_in_list = []
+            col_list_in_list = [e1 for e1 in [re.findall(regex["col_shortened"], colum) for colum in df_long.columns] if
+                                e1]
+            col_list = [item for sublist in col_list_in_list for item in sublist]
+            df = pd.read_csv(filename, sep="\t", comment="#", usecols=col_list)
+            return df
+
+        def filterdf_lfq(df, regex):
+            """"filtering the data in terms of removing entries caracterized by potential contaminants, etc.
+
+            Args:
+                df: raw dataframe as protein groups file, produced by MaxQuant
+                regex: dictionary, in which the values represent the regular expressions
+            """
+            df_filt = df.loc[df[[col for col in df.columns if bool(re.match(regex["filter1"], col))][0]] != "+"]
+            df_filt = df_filt.loc[df_filt[[col for col in df_filt.columns if
+                                           bool(re.match(regex["filter2"], col))][0]] != "+"]
+            df_filt = df_filt.loc[df_filt[[col for col in df_filt.columns if
+                                           bool(re.match(regex["filter3"], col))][0]] != "+"]
+
+            return df_filt
+
+        def indexingdf_lfq(df_filt, regex):
+            """
+
+            """
+            df_index = df_filt.copy()
+            col_to_index = [col for col in df_filt.columns if re.match(regex["index_col_lfq"], col)]
+            df_index = df_index.set_index(col_to_index)
+            df_index = df_index.drop([column for column in df_index.columns if not column.startswith(("LFQ", "MS"))],
+                                     axis=1)
+                                    #df_indexx.columns was there before
+            df_index[[col for col in df_index.columns if re.match(regex["LFQ_nan"], col)]] = df_index[[col
+                                for col in df_index.columns if re.match(regex["LFQ_nan"], col)]].replace(0, np.nan)
+
+            columns = pd.MultiIndex.from_arrays([
+                [re.findall(regex["Map_lfq"], colum)[0] for colum in df_index.columns],
+                [re.findall(regex["Fraction_lfq"], colum)[0] for colum in df_index.columns],
+                [re.findall(regex["Type_lfq"], colum)[0] for colum in df_index.columns]],
+                names=["Map", "Fraction", "Type"])
+            df_index.columns = columns
+
+            df_test = df_index.copy().stack("Fraction")
+            number_fractions = len(df_test.index.get_level_values("Fraction").unique())
+            # self.number_fractions=number_fractions
+
+            df_index = df_index.stack("Map")
+            df_index.columns = df_index.columns.swaplevel(0, 1)
+            df_index.sort_index(axis=1, level=0, inplace=True)
+
+            df_ms = df_index.loc[df_index[('MS/MS count')].apply(np.sum,axis=1) >= (
+                                             number_fractions * self.lfq_filter_param["summed MS/MS counts"])]
+            # for the column MS/MS count, you have to generate the sum, and if the sum is larger than n[fraction]*2, you take it into new f
+            df_lfq = df_ms.copy()
+            df_lfq = df_lfq.loc[df_lfq[("LFQ intensity")].apply(lambda x: any(
+                np.invert(np.isnan(x)).rolling(window=self.lfq_filter_param["consecutive LFQ_I"]).sum() >=
+                self.lfq_filter_param["consecutive LFQ_I"]), axis=1)]
+            # series no DF; if there are at least i.e. 4 consecutive non-NANs
+            df_norm = df_lfq["LFQ intensity"].div(df_lfq["LFQ intensity"].sum(axis=1), axis=0)
+            # 0-1 normalization but only for LFQ intensity
+            df_ms_lfq = df_lfq.copy()
+            df_ms_lfq["LFQ intensity"] = df_norm
+            # you overwrite the column ["LFQ intensity"] with df_norm, which contains only the data for LFQ intensity
+            df_ms_lfq = df_ms_lfq.fillna(0).stack("Fraction")
+            # NAN will be replaced with 0
+            df_ms_lfq.columns = [col if col != "LFQ intensity" else "normalized profile" for col in df_ms_lfq.columns]
+            # rename columns: "LFQ intensity" into "normalized profile"
+            return df_ms_lfq
+
+
         if acquisition == "SILAC":
-            df_filt = filterdf_silac(self.df, self.regex)
-            df_final = indexingdf_silac(df_filt, self.regex)
+            df_filter = filterdf_silac(self.df, self.regex)
+            df_final = indexingdf_silac(df_filter, self.regex)
             self.df_final = df_final
             fractions = df_final.index.get_level_values("Fraction").unique()
             self.fractions = fractions
             # fractions als return value
+            return df_final
 
-            return df_final.head()
 
-            # return df_processed
         elif acquisition == "LFQ":
-            return self.df.tail()
+            df = readingdf_lfq(self.filename, self.df_long, self.regex)
+            df_filter = filterdf_lfq(df, self.regex)
+            df_ms_lfq = indexingdf_lfq(df_filter, self.regex)
+            self.df_final = df_ms_lfq
+            return df_ms_lfq
+
         else:
             return "I don't know this"
 
@@ -120,10 +232,11 @@ class SpatialDataSet:
         df_plot = self.df_final
         markerproteins = self.markerproteins
         for marker in markerproteins["Proteasome"]:
-            plot_try = df_plot.xs((marker, "MAP1"), level=["Gene names", "Map"], drop_level=False)
+            plot_try = df_plot.xs((marker, "EGF_rep1"), level=["Gene names", "Map"], drop_level=False)
+     #       plot_try = df_plot.xs((marker, "MAP1"), level=["Gene names", "Map"], drop_level=False)
             plot_try = plot_try.reset_index()
             df_complex = df_complex.append(plot_try)
-        fig = px.line(df_complex, x="Fraction", y="Ratio H/L", color="Gene names", title='Relative abundance profile')
+        fig = px.line(df_complex, x="Fraction", y="normalized profile", color="Gene names", title='Relative abundance profile')
         return fig
 
 
