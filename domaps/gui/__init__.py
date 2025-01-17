0b300efb85929451f30675c2b2b0083bbf8c52ef
Julia Schessner <schessner@biochem.mpg.de>
2025-01-17 11:14:51 +0100

    Switch to pyproject.toml and split requirements

--- domaps/gui/__init__.py
+++ domaps/gui/__init__.py
@@ -1,1 +1,6 @@
-from .gui import *
+try:
+    from .gui import *
+except ImportError:
+    raise ImportError(
+        "GUI dependencies are not installed. Install them with 'pip install domaps[gui]'"
+    )
--- domaps/network/__init__.py
+++ domaps/network/__init__.py
@@ -1,1 +1,6 @@
-from .network import *
+try:
+    from .network import *
+except ImportError:
+    raise ImportError(
+        "GUI dependencies are not installed. Install them with 'pip install domaps[gui]'"
+    )
